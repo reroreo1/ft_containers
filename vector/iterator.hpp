3d9ba3455e6fa97cc8b15b3c2dc6f87b8e76d35d
Reroreo1 <rachid2pc1@gmail.com>
2023-01-25 00:34:12 +0100

    vector

---
+++ vector/iterator.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rezzahra <rezzahra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 22:56:35 by rezzahra          #+#    #+#             */
+/*   Updated: 2023/01/20 18:21:28 by rezzahra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
+
+template <typename T>
+
+class iterator{
+	private:
+		T* ptr;
+	public:
+		
+};
+
+#endif
